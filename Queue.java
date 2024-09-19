627886e45793c88dadd02302cbccfd884f6cf945
kael <kaelscott8@gmail.com>
2024-09-19 10:19:37 -0300

    add dequeue method

--- Queue.java
+++ Queue.java
@@ -5,13 +5,39 @@ public class Queue {
     private int[] data = new int[10];
 
     public Queue(int capacity) {
-//        data = new int[capacity];
+        //data = new int[capacity];
         data = new int[10];
         head = 0;
         tail = 0;
         size = 0;
     }
 
+    public void enqueue(int value) {
+        if (isFull()) {
+            throw new IllegalArgumentException("A Fila está cheia");
+        }
+        data[tail] = value; //add elemento na posicao do tail
+        tail++;
+        size++;
+
+    }
+
+    public void dequeue(){
+        if (isEmpty()){
+            throw new IllegalArgumentException("A fila esta vazia");
+        }
+        int removedValue = data[head];
+        data[head] = 0;
+        head++;
+        size--;
+    }
+
+    private boolean isFull(){
+        return size == data.length;
+    }
+    private boolean isEmpty(){
+        return size == 0;
+    }
 }
 
 
